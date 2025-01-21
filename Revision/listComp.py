39e34bae1a8b1a795821185b3cb693b1b1147dca
un1u3 <python.gitub.devs@gmail.com>
2025-01-21 22:29:18 +0545

    Nothing But Started

---
+++ Revision/listComp.py
@@ -0,0 +1,23 @@
+nums = [1,2,3,4,5,6,6]
+
+# my_list = []
+# for n in nums:
+#     my_list.append(n)
+
+# my_list =  [n for n in nums]
+
+
+# my_list = []
+# for n in nums:
+#     my_list.append(n*n)
+
+# my_list =  [n for n in nums]
+
+
+# my_list = [n*n for n in nums ]
+# print (my_list)
+
+
+myList = [n **n for n in nums]
+
+print(myList)
