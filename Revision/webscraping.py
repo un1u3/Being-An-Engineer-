99496bacf3e8a019f116289f6e45b2b749d42580
Your GitHub Username <python.github.devs@gmail.com>
2025-01-25 23:53:57 +0545

    addded a webscrapper

--- Revision/webscraping.py
+++ Revision/webscraping.py
@@ -1,9 +1,10 @@
 from bs4 import BeautifulSoup
 import requests
 
-# Open and parse the HTML file
-with open('simple.html') as html_file:  # Corrected typo here
-    soup = BeautifulSoup(html_file, 'lxml')
 
-# Print the parsed content
-print(soup)
+source = requests.get('http://coreyms.com').text
+
+soup = BeautifulSoup(source,'lxml')
+
+
+print(soup.prettify())
